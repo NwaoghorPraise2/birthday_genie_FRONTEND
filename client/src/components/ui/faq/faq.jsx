dc306b8c9fd40859db3405c1350cf1013e05fcc9
queencobham <queencobham@gmail.com>
2025-02-16 03:28:45 -0800

    Added Faq style

--- client/src/components/ui/faq/faq.jsx
+++ client/src/components/ui/faq/faq.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./faq.scss";
 
 const FAQ = () => {
   const faqs = [
@@ -28,52 +29,62 @@ const FAQ = () => {
   };
 
   return (
-    <div>
-      <h2>Frequently Asked Questions</h2>
-      {faqs.map((faq, index) => (
-        <div
-          key={index}
-          style={{
-            border: "1px solid #ccc",
-            padding: "10px",
-            marginBottom: "5px",
-            borderRadius: "5px",
-          }}
-        >
-          <div
-            onClick={() => toggleFAQ(index)}
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              cursor: "pointer",
-              fontWeight: "bold",
-            }}
-          >
-            {faq.question}
-            <span>{openFaq === index ? "x" : "+"}</span>
-          </div>
-          {openFaq === index && (
-            <div>
-              <p>{faq.answer}</p>
+    <div className="Faq">
+      <hr />
+      <div className="container">
+        <div className="text-container">
+          <h2>Frequently Asked Questions</h2>
+          <p>Still have any questions? Contact our Team via info@birthday.com</p>
+          <a href="#">See All FAQs</a>
+        </div>
+        <div className="item-container">
+          {faqs.map((faq, index) => (
+            <div
+              key={index}
+              style={{
+                border: "1px solid #ccc",
+                padding: "10px",
+                marginBottom: "5px",
+                borderRadius: "5px",
+              }}
+            >
               <div
+                onClick={() => toggleFAQ(index)}
                 style={{
-                  background: "#e0e0e0",
-                  padding: "5px",
-                  borderRadius: "5px",
-                  marginTop: "10px",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  cursor: "pointer",
+                  fontWeight: "bold",
                 }}
               >
-                <p>
-                  Not Satisfied?{" "}
-                  <button style={{ cursor: "pointer" }}>
-                    Send us a message now.
-                  </button>
-                </p>
+                {faq.question}
+                <span>{openFaq === index ? "x" : "+"}</span>
               </div>
+              {openFaq === index && (
+                <div>
+                  <p>{faq.answer}</p>
+                  <div
+                    style={{
+                      background: "#e0e0e0",
+                      padding: "5px",
+                      borderRadius: "5px",
+                      marginTop: "10px",
+                    }}
+                  >
+                    <p>
+                      Not Satisfied?{" "}
+                      <button style={{ cursor: "pointer" }}>
+                        Send us a message now.
+                      </button>
+                    </p>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
+          ))}
         </div>
-      ))}
+      </div>
+      <hr />
     </div>
   );
 };
